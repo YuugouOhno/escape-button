f389678c1bdc07ebe944c5f6e61a81d375569af9
YuugouOhno <yuugou.ohno@gmail.com>
2023-03-27 16:57:12 +0900

    マウスの座標に要素が集まる、移動の形式を絶対座標に変更

--- content_scripts.js
+++ content_scripts.js
@@ -4,21 +4,21 @@ for (let i = 0; i < all_a.length; i++) {
     console.log(all_a[i].text);
 }
 
-for (let i = 0; i < all_a.length; i++) {
-    let a = all_a[i];
-    a.addEventListener('mouseover', () => {
-        // console.log(i, "is hoverd")
-        // a.style.transform = 'translate(100px,100px)';
-        // a.style.setProperty('transform', 'translate(100px, 100px)', 'important');
-        a.style.cssText = 'transform: translate(30%, 30%)';
-        a.style.zIndex = '999';
-        a.style.position = 'fixed';
-        setTimeout(() => {
-            a.style.transform = 'initial';
-            a.style.zIndex = 'initial';
-        }, 200);
-    })
-}
+// for (let i = 0; i < all_a.length; i++) {
+//     let a = all_a[i];
+//     a.addEventListener('mouseover', () => {
+//         // console.log(i, "is hoverd")
+//         // a.style.transform = 'translate(100px,100px)';
+//         // a.style.setProperty('transform', 'translate(100px, 100px)', 'important');
+//         a.style.cssText = 'transform: translate(30%, 30%)';
+//         // a.style.zIndex = '999';
+//         // a.style.position = 'fixed';
+//         setTimeout(() => {
+//             a.style.transform = 'initial';
+//             a.style.zIndex = 'initial';
+//         }, 200);
+//     })
+// }
 for (let i = 0; i < all_button.length; i++) {
     let button = all_button[i];
     button.addEventListener('mouseout', () => {
