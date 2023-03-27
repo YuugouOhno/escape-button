de4d93c0c061b18901f76a227815aa20775b8b5e
YuugouOhno <yuugou.ohno@gmail.com>
2023-03-27 23:30:18 +0900

    モテたいけど、逃げられたい？ そんな君には“モテ逃げボタン”

--- content_scripts.js
+++ content_scripts.js
@@ -1,8 +1,8 @@
-const all_a = document.getElementsByTagName('a');
-const all_button = document.getElementsByTagName('button');
-for (let i = 0; i < all_a.length; i++) {
-    console.log(all_a[i].text);
-}
+// const all_a = document.getElementsByTagName('a');
+// const all_button = document.getElementsByTagName('button');
+// for (let i = 0; i < all_a.length; i++) {
+//     console.log(all_a[i].text);
+// }
 
 // for (let i = 0; i < all_a.length; i++) {
 //     let a = all_a[i];
@@ -19,15 +19,15 @@ for (let i = 0; i < all_a.length; i++) {
 //         }, 200);
 //     })
 // }
-for (let i = 0; i < all_button.length; i++) {
-    let button = all_button[i];
-    button.addEventListener('mouseout', () => {
-        // console.log(i, "is hoverd")
-        // button.style.transform = 'translate(100px,100px)';
-        a.style.zIndex = '2147483647';
-        setTimeout(() => {
-            button.style.transform = 'initial';
-            a.style.zIndex = 'initial';
-        }, 200);
-    })
-}
+// for (let i = 0; i < all_button.length; i++) {
+//     let button = all_button[i];
+//     button.addEventListener('mouseout', () => {
+//         // console.log(i, "is hoverd")
+//         // button.style.transform = 'translate(100px,100px)';
+//         a.style.zIndex = '2147483647';
+//         setTimeout(() => {
+//             button.style.transform = 'initial';
+//             a.style.zIndex = 'initial';
+//         }, 200);
+//     })
+// }
