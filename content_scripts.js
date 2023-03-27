8257341107cf520353a12001dd99117d729e7685
bamboo <minnie20000622@yahoo.co.jp>
2023-03-27 14:22:25 +0900

    Zindexを最大にし、いろんなボタンがいい感じに逃げている

--- content_scripts.js
+++ content_scripts.js
@@ -1,11 +1,33 @@
-const all_a = document.querySelectorAll('body a');
+const all_a = document.getElementsByTagName('a');
+const all_button = document.getElementsByTagName('button');
+for (let i = 0; i < all_a.length; i++) {
+    console.log(all_a[i].text);
+}
+
 for (let i = 0; i < all_a.length; i++) {
     let a = all_a[i];
     a.addEventListener('mouseover', () => {
-        console.log(i, "is hoverd")
-        a.style.transform = 'translate(100px,100px)';
+        // console.log(i, "is hoverd")
+        // a.style.transform = 'translate(100px,100px)';
+        // a.style.setProperty('transform', 'translate(100px, 100px)', 'important');
+        a.style.cssText = 'transform: translate(30%, 30%)';
+        a.style.zIndex = '999';
+        a.style.position = 'fixed';
         setTimeout(() => {
             a.style.transform = 'initial';
+            a.style.zIndex = 'initial';
+        }, 200);
+    })
+}
+for (let i = 0; i < all_button.length; i++) {
+    let button = all_button[i];
+    button.addEventListener('mouseout', () => {
+        // console.log(i, "is hoverd")
+        // button.style.transform = 'translate(100px,100px)';
+        a.style.zIndex = '999';
+        setTimeout(() => {
+            button.style.transform = 'initial';
+            a.style.zIndex = 'initial';
         }, 200);
     })
 }
@@ -14,10 +36,12 @@ const all_p = document.querySelectorAll('body p');
 for (let i = 0; i < all_p.length; i++) {
     let p = all_p[i];
     p.addEventListener('mouseover', () => {
-        console.log(i, "is hoverd")
-        p.style.transform = 'translate(5px,5px)';
+        // console.log(i, "is hoverd")
+        // p.style.transform = 'translate(5px,5px)';
+        a.style.zIndex = '999';
         setTimeout(() => {
             p.style.transform = 'initial';
+            a.style.zIndex = 'initial';
         }, 200);
     })
 }
